3ac130b38acf3692ec5d2747a52bcca6a19c3421
Dave Luhman <dave@ado.software>
2023-05-17 16:10:03 -0500

    removed old code, added debugger insertion

--- app.twoFaces.js
+++ app.twoFaces.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-
+debugger;
 const dictionary = fs.readFileSync(
   path.join(__dirname, "wordlist.10000"),
   "utf8"
@@ -20,30 +20,6 @@ function prepareDictionary(dictionary) {
 function splitWord(word) {
   //   console.log(word);
   return word.toLowerCase().split(""); // now it should look like ['d', 'a', 'v', 'e']
-}
-
-// Actual Implementation:
-
-function nestedIterator(nestedWord) {
-  const nestedWordAsArray = splitWord(nestedWord);
-  const E1 = nestedWordAsArray[0];
-  const F1 = nestedWordAsArray[1];
-  const G1 = nestedWordAsArray[2];
-  const H1 = nestedWordAsArray[3];
-  let newWordOne = B1 + A1 + E1 + F1;
-  let newWordTwo = C1 + D1 + H1 + G1;
-  if (wordList.includes(newWordOne) && wordList.includes(newWordTwo)) {
-    console.log(newWordOne + " " + newWordTwo);
-    return { "Word One": newWordOne, "Word Two": newWordTwo };
-  }
-}
-function topLevelIterator(wordList) {
-  const wordAsArray = splitWord(topLevelWord);
-  var A1 = wordAsArray[0];
-  var B1 = wordAsArray[1];
-  var C1 = wordAsArray[2];
-  var D1 = wordAsArray[3];
-  wordList.forEach(nestedIterator(wordList));
 }
 
 function main() {
@@ -66,8 +42,8 @@ function main() {
       let newWordOne = B1 + A1 + E1 + F1;
       let newWordTwo = C1 + D1 + H1 + G1;
       if (wordList.includes(newWordOne) && wordList.includes(newWordTwo)) {
+        result.push([newWordOne, newWordTwo]);
       }
-      result.push([newWordOne, newWordTwo]);
     }
     fs.writeFileSync("./result.csv", JSON.stringify(result));
   }
